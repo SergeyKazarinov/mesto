38f084dad186dacb445b4e11d265a2ebfbc3db06
Sergey Kazarinov <kazarinov092@gmail.com>
2022-05-09 17:29:40 +0500

    fix after the first iteration

--- sciprts/script.js
+++ sciprts/script.js
@@ -1,76 +1,26 @@
-const popup = document.querySelector ('.popup');
+/** модальные окна */
 const popupImageZoom = document.querySelector ('.popup_type_image-zoom');
 const popupEditProfile = document.querySelector ('.popup_type_edit-profile');
 const popupAddImage = document.querySelector ('.popup_type_add-image');
-
+/** кнопки */
 const popupEditOpenBtn = document.querySelector ('.button_type_edit');
 const popupAddImageOpenBtn = document.querySelector ('.button_type_add');
 const popupCloseBtnEditProfile = popupEditProfile.querySelector ('.button_type_close');
 const popupCloseBtnAddImage = popupAddImage.querySelector ('.button_type_close');
 const popupCloseBtnImage = popupImageZoom.querySelector ('.button_type_close');
-
+/** поля и формы */
 const nameInput = document.querySelector ('.form__input_type_name');
 const jobInput = document.querySelector ('.form__input_type_job');
 const profileName = document.querySelector ('.profile__name');
 const profileJob = document.querySelector ('.profile__job');
 const formProfileEdit = document.querySelector('form[name=profile-edit');
 const formAddImage = document.querySelector('form[name=add-image');
-
-//функция открытия попапа
-function addClassPopupEditProfile () {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-  popupEditProfile.classList.add('popup_opened');
-}
-
-function addClassAddImage () {
-  popupAddImage.classList.add('popup_opened');
-}
-
-function addClassImageZoom () {
-	popupImageZoom.classList.add('popup_opened');
-}
-
-//функция закрытия попапа
-function removeClass () {
-	if(popupEditProfile.classList.contains('popup_opened')) 
-  popupEditProfile.classList.remove('popup_opened');
-
-	else if(popupAddImage.classList.contains('popup_opened'))
-  popupAddImage.classList.remove('popup_opened');
-
-	else if(popupImageZoom.classList.contains('popup_opened'))
-	popupImageZoom.classList.remove('popup_opened');
-}
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Вставьте новые значения с помощью textContent
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    removeClass();
-}
-
-popupEditOpenBtn.addEventListener ('click', addClassPopupEditProfile); //событие открытия попапа редактирования профиля
-
-popupAddImageOpenBtn.addEventListener('click', addClassAddImage); //событие открытия попапа добавления картинки
-
-//Событие закрытия попапа
-popupCloseBtnEditProfile.addEventListener('click', removeClass);
-popupCloseBtnAddImage.addEventListener('click', removeClass);
-popupCloseBtnImage.addEventListener('click', removeClass);
-
-formProfileEdit.addEventListener('submit', formSubmitHandler); // он будет следить за событием “submit” - «отправка»
-//закрытие попапа кликом на любую облать
-// popup.addEventListener ('click', function (event) {
-//   if (event.target == popupFlex) {
-//     toggleClass();
-//   }
-// });
-///////////////////////////////////////////////////Практическая работа № 5
-//массив для заполнения страницы 6 фотографиями
+const placeInput = document.querySelector ('.form__input_type_place');
+const linkInput = document.querySelector ('.form__input_type_link');
+/** template и блок для вставки */
+const cardTemplate = document.querySelector('#cards').content;
+const newCard = document.querySelector('.elements__grid');
+/** массив для заполнения страницы 6 фотографиями */
 const initialCards = [
   {
     name: 'Скала у берега',
@@ -98,73 +48,115 @@ const initialCards = [
   }
 ];
 
-const cardTemplate = document.querySelector('#cards').content;
-const cardOnline = document.querySelector('.elements__grid');
+/** функция заполнения форм редактирования */
+const fillForm = () => {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
 
-//Удаление карточки
+/** функция открытия попапа */
+const addClass = (event) => {
+	event.classList.add('popup_opened');
+}
+
+/** функция закрытия попапа */
+const removeClass = event => {
+	event.classList.remove('popup_opened');
+}
+
+/** Удаление карточки */
 const removeImage = evt => {
   evt.target.closest('.card').remove();
 }
 
-//лайк
+/** лайк */
 const like = event => {
 	event.target.classList.toggle('button_type_like-active');
 }
 
-//открытие попап с картинкой
+/** открытие попап с картинкой */
 const openPopupImage = event => {
 	const image = popupImageZoom.querySelector('.popup__image');
 	image.src = event.target.closest('.card__image').src;
 	image.alt = event.target.closest('.card__image').alt;
 	popupImageZoom.querySelector('.popup__text').textContent = event.target.closest('.card__image').alt;
 
-	addClassImageZoom();
+	addClass(popupImageZoom);
 }
 
-let cardElement;
-
-const actionBtn = () => {
-  cardElement.querySelector('.button_type_like').addEventListener ('click', like); //лайк
-	cardElement.querySelector('.button_type_remove').addEventListener('click', removeImage); //Удаление картинки
-	cardElement.querySelector('.button_type_card').addEventListener('click', openPopupImage) //открытие картинки
-}
-
-//заполнение сайта шестью карточками
-initialCards.forEach(item => {
-
-  cardElement = cardTemplate.querySelector('.card').cloneNode(true); //клонирование содержимое тега template
-
-  //заполнение template данными
+/** функция заполяет карточки данными */
+const createCard = (name, link) => {
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 	const cardImage = cardElement.querySelector('.card__image');
 	const cardText = cardElement.querySelector('.card__text');
-  cardImage.src = item.link;
-  cardImage.alt = item.name;
-  cardText.textContent = item.name;
 
-	actionBtn();
+	cardImage.src = link;
+	cardImage.alt = name;
+	cardText.textContent = name;
 
-  cardOnline.append(cardElement); //добавление блока на сайт
+	cardElement.querySelector('.button_type_like').addEventListener ('click', like); //лайк
+	cardElement.querySelector('.button_type_remove').addEventListener('click', removeImage); //Удаление картинки
+	cardElement.querySelector('.button_type_card').addEventListener('click', openPopupImage); //открытие картинки
+
+	return cardElement;
+}
+
+/** функция добавления карточки в контейнер */
+const renderCard = (wrap, name, link) => {
+	wrap.append(createCard(name, link));
+}
+
+/** заполнение сайта шестью карточками */
+initialCards.forEach((item) => {
+  renderCard(newCard, item.name, item.link);
 })
 
-//Добавление картинки
-const placeInput = document.querySelector ('.form__input_type_place');
-const linkInput = document.querySelector ('.form__input_type_link');
-const makeBtn = document.querySelector ('.button_type_make');
-
-function addImage (evt) {
-  evt.preventDefault();
-  cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-	const cardImage = cardElement.querySelector('.card__image');
-	const cardText = cardElement.querySelector('.card__text');
-  cardImage.src = linkInput.value;
-  cardImage.alt = placeInput.value;
-  cardText.textContent = placeInput.value;
-	linkInput.value = '';
-	placeInput.value = '';
-	actionBtn();
-  cardOnline.prepend(cardElement);  //добавление блока на сайт
-
-  removeClass();
+/** Добавление картинки */
+const addCard = (wrap, name, link) => {
+	wrap.prepend(createCard(name, link));
 }
 
+const addImage = evt => {
+  evt.preventDefault();
+	addCard(newCard, placeInput.value, linkInput.value);
+	formAddImage.reset();
+	removeClass(popupAddImage);
+}
+
+/** Обработчик «отправки» формы, хотя пока
+она никуда отправляться не будет */
+function formEditProfileSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+
+    removeClass(popupEditProfile);
+}
+
+/** событие открытия попапа редактирования профиля */
+popupEditOpenBtn.addEventListener ('click', () => {
+	fillForm();
+	addClass(popupEditProfile);
+});
+
+/** событие открытия попапа добавления картинки */
+popupAddImageOpenBtn.addEventListener('click', () => {
+	addClass(popupAddImage);
+});
+
+/** События закрытия попапа */
+popupCloseBtnEditProfile.addEventListener('click', () => {
+	removeClass(popupEditProfile);
+});
+popupCloseBtnAddImage.addEventListener('click', () => {
+	removeClass(popupAddImage);
+});
+popupCloseBtnImage.addEventListener('click', () => {
+	removeClass(popupImageZoom);
+});
+/** событие отправки формы с именем */
+formProfileEdit.addEventListener('submit', formEditProfileSubmitHandler);
+
+/** событие добавление картинки на сайт */
 formAddImage.addEventListener('submit', addImage);
